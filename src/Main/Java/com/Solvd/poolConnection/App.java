5dd86be33a94862da30bd6faa631cfba57685c28
Luciano <luciano1231@hotmail.com>
2020-05-05 05:25:13 -0300

    Ignored settigns folder

--- src/Main/Java/com/Solvd/poolConnection/App.java
+++ src/Main/Java/com/Solvd/poolConnection/App.java
@@ -3,13 +3,11 @@ package main.java.com.solvd.poolConnection;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 
 public class App 
 {	
-	private static final Logger LOG = LogManager.getLogger(App.class);
+	
     public static void main( String[] args )
     {
     	ConnectionPool connectionPool = ConnectionPool.getInstance(10);
